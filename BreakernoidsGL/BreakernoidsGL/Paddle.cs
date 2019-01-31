9b551f87bbcfc6a4bcc332ecfe1b64584d6ead75
Unknown <kkinney2@student.fitchburgstate.edu>
2019-01-31 11:18:57 -0500

    File Commit
    Part1

---
+++ BreakernoidsGL/BreakernoidsGL/Paddle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace BreakernoidsGL
+{
+    public class Paddle : GameObject
+    {
+        public float speed;
+
+        public Paddle(Game myGame) : base(myGame)
+        {
+            textureName = "paddle";
+        }
+
+        public override void Update(float deltaTime)
+        {
+            base.Update(deltaTime);
+
+            KeyboardState keyState = Keyboard.GetState();
+
+            if (keyState.IsKeyDown(Keys.Left))
+            {
+                position.X -= speed * deltaTime;
+            }
+            else if (keyState.IsKeyDown(Keys.Right))
+            {
+                position.X += speed * deltaTime;
+            }
+        }
+    }
+}
